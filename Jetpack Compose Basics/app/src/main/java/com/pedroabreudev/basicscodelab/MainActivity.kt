9712f006c8a400244df31d5ed6f330536cd70d3b
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-05-15 23:15:08 -0400

    Styling and theming your app

--- Jetpack Compose Basics/app/src/main/java/com/pedroabreudev/basicscodelab/MainActivity.kt
+++ Jetpack Compose Basics/app/src/main/java/com/pedroabreudev/basicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pedroabreudev.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pedroabreudev.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -99,7 +101,9 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             OutlinedButton(
                 onClick = { expanded = !expanded }
@@ -109,6 +113,13 @@ private fun Greeting(name: String) {
         }
     }
 }
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
